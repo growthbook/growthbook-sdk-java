c171a660a6bb809a915366127ffb7199d54e805f
tinahollygb <tina@growthbook.io>
2022-10-14 14:09:02 -0700

    FNV attempts

--- lib/src/main/java/growthbook/sdk/java/services/GrowthBookUtils.java
+++ lib/src/main/java/growthbook/sdk/java/services/GrowthBookUtils.java
@@ -3,14 +3,56 @@ package growthbook.sdk.java.services;
 import java.math.BigInteger;
 
 public class GrowthBookUtils {
+    // Like this: https://github.com/growthbook/growthbook-kotlin/blob/main/GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/Utils/GBUtils.kt#L50-L54
     public static Float hash(String stringValue) {
         BigInteger bigInt = MathUtils.fnv1a_32(stringValue.getBytes());
-//        BigInteger bigInt = MathUtils.fnv1a_32(stringValue.getBytes(StandardCharsets.UTF_8));
-        float floatValue = bigInt.floatValue();
-
         BigInteger thousand = new BigInteger("1000");
-        float thousandFloat = thousand.floatValue();
+        BigInteger remainder = bigInt.remainder(thousand);
 
-        return (floatValue % thousandFloat) / thousandFloat;
+        String remainderAsString = remainder.toString();
+        float remainderAsFloat = Float.parseFloat(remainderAsString);
+        return remainderAsFloat / 1000f;
     }
+
+    // Like this: https://github.com/jakedouglas/fnv-java/blob/master/src/main/java/com/bitlove/FNV.java#L35-L44
+//    public static Float hash(String stringValue) {
+//        BigInteger bigInt = MathUtils.fnv1a_32(stringValue.getBytes());
+////        BigInteger bigInt = MathUtils.fnv1a_32(stringValue.getBytes(StandardCharsets.UTF_8));
+//        float floatValue = bigInt.floatValue();
+//
+//        BigInteger thousand = new BigInteger("1000");
+//        float thousandFloat = thousand.floatValue();
+//
+//        return (floatValue % thousandFloat) / thousandFloat;
+//    }
+
+    // Like this: https://github.com/growthbook/growthbook-kotlin/blob/main/GrowthBook/src/commonMain/kotlin/com/sdk/growthbook/Utils/GBUtils.kt#L50-L54
+//    public static Float hash(String stringValue) {
+//        BigInteger bigInt = MathUtils.fnv1a_32(stringValue.getBytes());
+//        BigInteger thousand = new BigInteger("1000");
+//        BigInteger remainder = bigInt.remainder(thousand);
+//
+//        String remainderAsString = remainder.toString();
+//        float remainderAsFloat = Float.parseFloat(remainderAsString);
+//        return remainderAsFloat / 1000f;
+//    }
+
+    // From here: https://github.com/prasanthj/hasher/blob/master/src/main/java/hasher/FNV1a.java
+//    public static Float hash(String stringValue) {
+//        int i = MathUtils.hash32(stringValue.getBytes());
+////        BigInteger bigInt = MathUtils.fnv1a_32(stringValue.getBytes(StandardCharsets.UTF_8));
+//        float floatValue = (float) i;
+//
+//        float thousand = 1000f;
+////        BigInteger thousand = new BigInteger("1000");
+////        float thousandFloat = thousand.floatValue();
+//
+//        return (floatValue % thousand) / thousand;
+//    }
+
+    // http://www.java2s.com/Code/Java/Development-Class/FNVHash.htm
+//    public static Float hash(String stringValue) {
+//        int i = FNVHash.hash32(stringValue);
+//        return (float) i;
+//    }
 }
--- lib/src/main/java/growthbook/sdk/java/services/MathUtils.java
+++ lib/src/main/java/growthbook/sdk/java/services/MathUtils.java
@@ -42,4 +42,29 @@ public class MathUtils {
 
         return hash;
     }
+
+    // https://github.com/prasanthj/hasher/blob/master/src/main/java/hasher/FNV1a.java
+    public static int hash32(byte[] data) {
+        return hash32(data, data.length);
+    }
+
+    private static final int FNV1_32_INIT = 0x811c9dc5;
+    private static final int FNV1_PRIME_32 = 16777619;
+
+    /**
+     * FNV1a 32 bit variant.
+     *
+     * @param data   - input byte array
+     * @param length - length of array
+     * @return - hashcode
+     */
+    public static int hash32(byte[] data, int length) {
+        int hash = FNV1_32_INIT;
+        for (int i = 0; i < length; i++) {
+            hash ^= (data[i] & 0xff);
+            hash *= FNV1_PRIME_32;
+        }
+
+        return hash;
+    }
 }
