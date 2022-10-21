0dbe6990be16b1b0eb97905ab297adbff0d99806
tinahollygb <tina@growthbook.io>
2022-10-21 11:19:04 -0700

    stub GB lib class

--- lib/src/main/java/growthbook/sdk/java/GrowthBook.java
+++ lib/src/main/java/growthbook/sdk/java/GrowthBook.java
@@ -1,6 +1,39 @@
 package growthbook.sdk.java;
 
+import growthbook.sdk.java.models.Experiment;
+import growthbook.sdk.java.models.ExperimentResult;
+import growthbook.sdk.java.models.ExperimentRunCallback;
+
 public class GrowthBook implements IGrowthBook {
+    @Override
+    public ExperimentResult run(Experiment experiment) {
+        // TODO:
+        return null;
+    }
+
+    @Override
+    public Boolean isOn(String featureKey) {
+        // TODO:
+        return null;
+    }
+
+    @Override
+    public Boolean isOff(String featureKey) {
+        // TODO:
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        // TODO:
+    }
+
+    @Override
+    public void subscribe(ExperimentRunCallback callback) {
+        // TODO:
+    }
+
     // TODO: private getFeatureResult(value, source, experiment, experimentResult): FeatureResult
     // TODO: private getExperimentResult(experiment, variationIndex, hashUsed, featureId): ExperimentResult
+    // TODO: getFeatureValue(key, defaultValue) // defaultValue is the fallback value
 }
--- lib/src/main/java/growthbook/sdk/java/IGrowthBook.java
+++ lib/src/main/java/growthbook/sdk/java/IGrowthBook.java
@@ -1,12 +1,20 @@
 package growthbook.sdk.java;
 
+import growthbook.sdk.java.models.Experiment;
+import growthbook.sdk.java.models.ExperimentResult;
+import growthbook.sdk.java.models.ExperimentRunCallback;
+
 public interface IGrowthBook {
+    // TODO: Maybe not make it generic
+//    public FeatureResult evalFeature(String key, );
     // TODO: public evalFeature(key: string): FeatureResult
-    // TODO: public run(experiment: Experiment): ExperimentResult
-    // TODO: isOn(String featureKey)
-    // TODO: isOff(String featureKey)
-    // TODO: getFeatureValue(key, defaultValue) // defaultValue is the fallback value
-    // TODO: subscribe
-    // TODO: unsubscribe
+
+    public ExperimentResult run(Experiment experiment);
+    public Boolean isOn(String featureKey);
+    public Boolean isOff(String featureKey);
+
+    public void destroy();
+
+    public void subscribe(ExperimentRunCallback callback);
     // TODO: getAllResults (not required)
 }
