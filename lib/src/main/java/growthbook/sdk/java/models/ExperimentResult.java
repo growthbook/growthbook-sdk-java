7cc62025d95e3986032abfb4d377bb7ba70876b6
tinahollygb <tina@growthbook.io>
2022-10-25 15:42:06 -0700

    ExperimentResult

--- lib/src/main/java/growthbook/sdk/java/models/ExperimentResult.java
+++ lib/src/main/java/growthbook/sdk/java/models/ExperimentResult.java
@@ -1,5 +1,27 @@
 package growthbook.sdk.java.models;
 
-// TODO: ExperimentResult
-public class ExperimentResult {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.annotation.Nullable;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ExperimentResult<ValueType> {
+    @Nullable
+    ValueType value;
+
+    @Nullable
+    Integer variationId;
+
+    @Builder.Default
+    Boolean inExperiment = false;
+
+    @Nullable
+    Boolean hashUsed;
+
+    @Nullable
+    String featureId;
 }
