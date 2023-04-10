2eab3209127426f4040976602cb3a06bc2128917
tinahollygb <tina@growthbook.io>
2023-04-10 15:27:07 -0700

    add new class Filter w/ tests

---
+++ lib/src/main/java/growthbook/sdk/java/Filter.java
@@ -0,0 +1,42 @@
+package growthbook.sdk.java;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Object used for mutual exclusion and filtering users out of experiments based on random hashes.
+ */
+@Getter
+public class Filter {
+    String seed;
+
+    List<BucketRange> ranges;
+
+    String attribute;
+
+    HashVersion hashVersion;
+
+    /**
+     * Object used for mutual exclusion and filtering users out of experiments based on random hashes.
+     * @param seed The seed used in the hash
+     * @param ranges Array of ranges that are included
+     * @param attribute The attribute to use (default: "id")
+     * @param hashVersion The hash version to use (default: 2)
+     */
+    @Builder
+    public Filter(
+        @Nullable String seed,
+        List<BucketRange> ranges,
+        String attribute,
+        HashVersion hashVersion
+    ) {
+        this.seed = seed == null ? "" : seed;
+        this.ranges = ranges == null ? new ArrayList<>() : ranges;
+        this.attribute = attribute == null ? "id" : attribute;
+        this.hashVersion = hashVersion == null ? HashVersion.V2 : hashVersion;
+    }
+}
---
+++ lib/src/test/java/growthbook/sdk/java/FilterTest.java
@@ -0,0 +1,61 @@
+package growthbook.sdk.java;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterTest {
+    @Test
+    void test_canBeBuilt() {
+        ArrayList<BucketRange> ranges = new ArrayList<>();
+        ranges.add(new BucketRange(0.f, 0.2f));
+        ranges.add(new BucketRange(0.2f, 0.5f));
+
+        Filter subject = Filter.builder()
+            .attribute("user_id")
+            .seed("some-seed")
+            .hashVersion(HashVersion.V1)
+            .ranges(ranges)
+            .build();
+
+        assertEquals("user_id", subject.getAttribute());
+        assertEquals("some-seed", subject.getSeed());
+        assertEquals(HashVersion.V1, subject.getHashVersion());
+        assertEquals(ranges, subject.getRanges());
+    }
+
+    @Test
+    void test_canBeBuilt_defaults() {
+        Filter subject = Filter.builder().build();
+
+        assertEquals("id", subject.getAttribute());
+        assertEquals("", subject.getSeed());
+        assertEquals(HashVersion.V2, subject.getHashVersion());
+        assertEquals(0, subject.getRanges().size());
+    }
+
+    @Test
+    void test_canBeConstructed() {
+        ArrayList<BucketRange> ranges = new ArrayList<>();
+        ranges.add(new BucketRange(0.f, 0.2f));
+
+        Filter subject = new Filter("seed", ranges, "user_id", HashVersion.V1);
+
+        assertEquals("user_id", subject.getAttribute());
+        assertEquals("seed", subject.getSeed());
+        assertEquals(HashVersion.V1, subject.getHashVersion());
+        assertEquals(1, subject.getRanges().size());
+    }
+
+    @Test
+    void test_canBeConstructed_defaults() {
+        Filter subject = new Filter(null, null, null, null);
+
+        assertEquals("id", subject.getAttribute());
+        assertEquals("", subject.getSeed());
+        assertEquals(HashVersion.V2, subject.getHashVersion());
+        assertEquals(0, subject.getRanges().size());
+    }
+}
