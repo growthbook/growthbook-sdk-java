26ce0a3d885f8a09e34e9cdcb9f95e52b56ee3e3
vazarkevych <vazarkevych@users.noreply.github.com>
2024-05-27 11:06:36 +0000

    deploy: 4a096099495091569c8507312146d32c7692bbfa

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"growthbook.sdk.java"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"growthbook.sdk.java"},{"l":"growthbook.sdk.java.stickyBucketing"}];updateSearchResults();
