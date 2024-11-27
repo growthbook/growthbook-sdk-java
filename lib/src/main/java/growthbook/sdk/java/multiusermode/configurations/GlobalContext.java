7e59305a3f2ffd2d7d5ebd214822b9d9418783ec
Madhu Chavva <madhu.chavva@gmail.com>
2024-11-26 20:19:31 -0800

    Use global context at instance level

--- lib/src/main/java/growthbook/sdk/java/multiusermode/configurations/GlobalContext.java
+++ lib/src/main/java/growthbook/sdk/java/multiusermode/configurations/GlobalContext.java
@@ -5,6 +5,7 @@ import growthbook.sdk.java.Experiment;
 import growthbook.sdk.java.multiusermode.util.TransformationUtil;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Nullable;
@@ -15,34 +16,17 @@ import java.util.List;
 @Slf4j
 public class GlobalContext {
 
+
     /**
      * Keys are unique identifiers for the features and the values are Feature objects.
      * Feature definitions - To be pulled from API / Cache
      */
+    @Getter
     @Nullable
-    private JsonObject features; // do you need features Json? -- How is it handled in JS SDK?
-
-    @Nullable
-    private String featuresJson;
+    private JsonObject features;
 
     @Nullable
     private JsonObject savedGroups;
 
     private List<Experiment> experiments;
-
-    public JsonObject getFeatures() {
-        if (this.features == null) {
-            this.features = TransformationUtil.transformFeatures(this.featuresJson);
-            // Both are constructed then n there at the time of processing. Restructure them?
-            // TBD:M read saved Groups
-            // TBD:M experiments
-        }
-        return this.features;
-    }
-
-    public void loadEncryptedFeatures(String decryptionKey) {
-        if (this.features == null) {
-            this.features = TransformationUtil.transformEncryptedFeatures(this.featuresJson, decryptionKey);
-        }
-    }
 }
