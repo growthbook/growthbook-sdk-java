2e8d3fe689ecb87b24c04241f1a886672f7ba231
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsoulutions.com>
2025-02-27 12:47:52 +0200

    update Version

--- lib/src/main/java/growthbook/sdk/java/Version.java
+++ lib/src/main/java/growthbook/sdk/java/Version.java
@@ -4,5 +4,5 @@ package growthbook.sdk.java;
  * Tag for the published GrowthBook SDK version
  */
 public interface Version {
-    String SDK_VERSION = "0.9.94";
+    String SDK_VERSION = "0.9.96";
 }
