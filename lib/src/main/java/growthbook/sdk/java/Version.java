6457ff09e8a23ed243efceabced27d21e6880fc6
Madhu Chavva <46016208+madhuchavva@users.noreply.github.com>
2025-04-04 14:52:34 -0700

    fix: release-type java-yoshi dep errors (#138)

--- lib/src/main/java/growthbook/sdk/java/Version.java
+++ lib/src/main/java/growthbook/sdk/java/Version.java
@@ -7,5 +7,7 @@ package growthbook.sdk.java;
  */
 public class Version {
     // The current version of the GrowthBook SDK for Java
+    // x-release-please-start-version
     public static final String SDK_VERSION = "0.9.97";
+    // x-release-please-end
 }
